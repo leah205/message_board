380049bf40b4335a73cb2355529b01d809257488
leah205 <leahtiktin@gmail.com>
2025-07-21 11:22:22 -0600

    add font

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const {indexRouter} = require('./routes/index.js')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
+let staticPaths = path.join(__dirname, 'public')
+app.use(express.static(staticPaths))
 PORT = 3000
 
 app.use("/", indexRouter)
